d216fc2e9dc2a2048cf41936a62adcf1702729df
Vazguen <marvazgen@gmail.com>
2022-03-15 02:45:20 +0100

    first commit

---
+++ src/main/kotlin/SomeException.kt
@@ -0,0 +1,11 @@
+class SomeException : Exception{
+    constructor() : super()
+    constructor(message: String) : super(message)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+    constructor(cause: Throwable) : super(cause)
+
+    init {
+        println("SomeException")
+    }
+
+}
